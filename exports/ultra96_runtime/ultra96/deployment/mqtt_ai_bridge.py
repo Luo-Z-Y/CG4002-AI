769216b395ae34b560bd53b6bb5248ab8a3f3d0f
Zhiyang <e0969568@u.nus.edu>
2026-04-12 13:10:12 +0800

    clean up code

--- exports/ultra96_runtime/ultra96/deployment/mqtt_ai_bridge.py
+++
@@ -1,5 +0,0 @@
-from deployment import main
-
-
-if __name__ == "__main__":
-    main()
--- exports/windows_hls/voice_new/voice_cnn.h
+++
@@ -1,8 +0,0 @@
-#ifndef VOICE_CNN_H
-#define VOICE_CNN_H
-
-#include "voice_typedefs.h"
-
-void voice_cnn(hls::stream<axis_t> &in_stream, hls::stream<axis_t> &out_stream);
-
-#endif
